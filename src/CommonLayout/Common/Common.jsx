ec6438b82413a0ee524e14f89cde1fd3b1c30c9d
shamimsikder <shamimsikder138@gmail.com>
2023-05-02 00:42:47 +0600

    added footer

--- src/CommonLayout/Common/Common.jsx
+++ src/CommonLayout/Common/Common.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../../CommonLayout/Navbar.jsx/Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 
 const Common = () => {
     return (
         <div>
             <Navbar/>
             <Outlet/>
+            <Footer/>
         </div>
     );
 };
--- src/CommonLayout/Navbar.jsx/Navbar.jsx
+++ src/CommonLayout/Navbar.jsx/Navbar.jsx
@@ -3,14 +3,14 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-sky-600 text-white">
             
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu  menu-compact dropdown-content mt-3 p-2 shadow bg-sky-500 rounded-box w-52">
                         <li><NavLink className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : "" } to='/'>Home</NavLink ></li>
                         <li><Link to='/blog'>Blogs</Link ></li>
                         <li><Link to='/home'>Home</Link ></li>
@@ -33,7 +33,9 @@ const Navbar = () => {
                 <div className="w-10 rounded-full">
                     <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
                 </div>
-                <Link className="btn" to='/login'>Login</Link>
+                <button className="px-3 py-2 border-[1px] border-[text-white] rounded-md">
+                    <Link to='/login'>Login</Link>
+                </button>
             </div>
 
         </div>
