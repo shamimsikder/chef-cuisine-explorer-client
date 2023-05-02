628397c8822047c58441516a8821d967a33b1450
shamimsikder <shamimsikder138@gmail.com>
2023-05-02 13:48:32 +0600

    fixed login form responsive issue

--- src/Layout/Login/Login.jsx
+++ src/Layout/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaGoogle, FaGithub } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 
@@ -15,27 +16,29 @@ const Login = () => {
                     <h2 className="text-2xl font-bold mb-6">Login</h2>
                     <div className="mb-4">
                         <label htmlFor="email" className="w-full text-gray-700 font-bold mb-2">Email</label>
-                        <input type="email" name="email" className="w-full border-gray-400 border-2 p-3 rounded-md" />
+                        <input type="email" name="email" placeholder='Enter Your Email' className="w-full border-gray-400 border-2 p-3 rounded-md" />
                     </div>
                     <div className="mb-6">
                         <label htmlFor="password" className="w-full text-gray-700 font-bold mb-2">Password</label>
-                        <input type="password" name="password" className="w-full border-gray-400 border-2 p-3 rounded-md" />
+                        <input type="password" name="password" placeholder='Enter Your Password' className="w-full border-gray-400 border-2 p-3 rounded-md" />
                     </div>
                     <button type="submit" className="w-full bg-blue-500 text-white p-3 rounded-md hover:bg-blue-600 mb-6">Login</button>
                 </form>
 
                 <p className='font-semibold text-xl text-gray-700'>or Login With</p>
 
-                <div className="w-[82%] bg-[#C54238] text-white p-3 rounded-md mt-6 mb-6">
+                <div className="w-full md:w-[82%] bg-[#C54238] text-white p-3 rounded-md mt-6 mb-6">
                     <button className='flex justify-center items-center gap-10'>
                         <FaGoogle/> <span>Sign in With Google</span>
                     </button>
                 </div>
-                <div className="w-[82%] bg-[black] text-white p-3 rounded-md">
+                <div className="w-full md:w-[82%] bg-[black] text-white p-3 rounded-md">
                     <button className='flex justify-center items-center gap-10'>
                         <FaGithub/> Sign in With Github
                     </button>
                 </div>
+
+                <p className='mb-2 mt-4'>Not a member? <Link to='/register' className='text-blue-500'>Sign Up</Link></p>
 
             </div>
 
