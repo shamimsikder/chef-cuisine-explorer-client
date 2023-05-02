ec9eecb9b50f37176e81b299894984f510bd19d3
shamimsikder <shamimsikder138@gmail.com>
2023-05-03 02:41:19 +0600

    fixed nav-bar issue and added banner

--- src/CommonLayout/Navbar.jsx/Navbar.jsx
+++ src/CommonLayout/Navbar.jsx/Navbar.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import './NavBar.css'
 
 const Navbar = () => {
     return (
-        <div className="navbar w-full max-w-6xl mx-auto text-black">
+        <div className="navbar w-full max-w-7xl mx-auto text-black">
             
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu  menu-compact dropdown-content mt-3 p-2 shadow  rounded-box w-52">
-                        <li><NavLink className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : "" } to='/'>Home</NavLink ></li>
-                        <li><NavLink className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : "" } to='/about'>About</NavLink ></li>
-                        <li><NavLink className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : "" } to='/blog'>Blogs</NavLink ></li>
-                        <li><NavLink className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : "" } to='/blogs'>Blogs</NavLink ></li>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-sky-50 rounded-box w-52">
+                        <li><NavLink className={({ isActive, isPending }) => isActive ? "active-link" : isPending ? "pending" : "" } to='/'>Home</NavLink ></li>
+                        <li><NavLink className={({ isActive, isPending }) => isActive ? "active-link" : isPending ? "pending" : "" } to='/about'>About</NavLink ></li>
+                        <li><NavLink className={({ isActive, isPending }) => isActive ? "active-link" : isPending ? "pending" : "" } to='/blog'>Blogs</NavLink ></li>
+                        <li><NavLink className={({ isActive, isPending }) => isActive ? "active-link" : isPending ? "pending" : "" } to='/blogs'>Blogs</NavLink ></li>
                     </ul>
                 </div>
-                <Link to='/' className=" font-semibold text-2xl">Chef <span className='text-sky-500'>Cuisine</span> Explorer</Link>
+                <Link to='/' className=" font-semibold text-2xl">Chef <span className='text-[#31B567]'>Cuisine</span> Explorer</Link>
             </div>
             
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><NavLink className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : "" } to='/'>Home</NavLink></li>
-                    <li><NavLink className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : "" } to='/about'>About</NavLink></li>
-                    <li><NavLink className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : "" } to='/blog'>Blogs</NavLink></li>
-                    <li><NavLink className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : "" } to='/blogs'>Blogs</NavLink></li>
+                <ul className="menu gap-2 menu-horizontal px-1">
+                    <li><NavLink className={({ isActive, isPending }) => isActive ? "active-link" : isPending ? "pending" : "" } to='/'>Home</NavLink></li>
+                    <li><NavLink className={({ isActive, isPending }) => isActive ? "active-link" : isPending ? "pending" : "" } to='/about'>About</NavLink></li>
+                    <li><NavLink className={({ isActive, isPending }) => isActive ? "active-link" : isPending ? "pending" : "" } to='/blog'>Blogs</NavLink></li>
+                    <li><NavLink className={({ isActive, isPending }) => isActive ? "active-link" : isPending ? "pending" : "" } to='/blogs'>Blogs</NavLink></li>
                 </ul>
             </div>
 
@@ -33,7 +34,7 @@ const Navbar = () => {
                 <div className="w-10 rounded-full">
                     <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
                 </div>
-                <button className="px-3 py-2 bg-sky-500 text-white rounded-md">
+                <button className="px-4 py-2 bg-[#31B567] text-white rounded-md">
                     <Link to='/login'>Login</Link>
                 </button>
             </div>
--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import banner from '../../assets/banner.jpg'
 
 const Banner = () => {
     return (
-        <div className=''>
+        <div className='w-full max-w-7xl mx-auto bg-sky-50 rounded-md mt-10'>
 
-            <div>
+            <div className='grid grid-cols-1 justify-center items-center gap-5 md:grid-cols-2'>
+
+                <div className=' order-last md:order-first p-4'>
+                    <h1 className='text-4xl font-semibold mb-3'>Experience the Authentic <br /> Taste  of <br />Italy</h1>
+                    <p>Indulge in the rich flavors and exquisite dishes of Italian cuisine, made with the freshest ingredients and traditional recipes.</p>
+                    <p className='mt-3'>Join us for a taste of la dolce vita.</p>
+                    <button class="mt-3 px-4 py-2 bg-[#31B567] text-white rounded-md">Get Started</button>
+                </div>
+                <div>
+                    <img className='rounded-md' src={banner} alt="" />
+                </div>
 
             </div>
 
